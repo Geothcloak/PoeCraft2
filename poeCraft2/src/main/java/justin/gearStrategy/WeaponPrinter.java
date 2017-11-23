98fa4853ab61b6240ae14832f702dd03d9ae4b09
geothcloak <justinroesner@gmail.com>
2017-11-23 12:51:32 -0600

    finished some mocks.

--- poeCraft2/src/main/java/justin/gearStrategy/WeaponPrinter.java
+++ poeCraft2/src/main/java/justin/gearStrategy/WeaponPrinter.java
@@ -8,7 +8,7 @@ public class WeaponPrinter implements Strategy{
 	public String printGear(Gear gear) {
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append("Hey you used a weapon! " + ((Weapon) gear).getType());
-		stringBuilder.append(" its name is" + ((Weapon) gear).getName());
+		stringBuilder.append(" its name is " + ((Weapon) gear).getName());
 		System.out.println(stringBuilder);
 		
 		return stringBuilder.toString();
--- poeCraft2/src/test/java/justin/gearStrategy/WeaponPrinterTest.java
+++ poeCraft2/src/test/java/justin/gearStrategy/WeaponPrinterTest.java
@@ -1,18 +1,33 @@
 package justin.gearStrategy;
 
+import org.easymock.EasyMock;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class WeaponPrinterTest {
-	//TODO mock a weapon
-	//TODO print weapon to pass asserts
+import justin.gearFactory.Weapon;
+
+public class WeaponPrinterTest {	
+	WeaponPrinter weaponPrinter;
+	Weapon mockWeapon;
 	
+	//TODO research apptest / config file for mocking needed?
 	@Before
 	public void init(){
-		
+		mockWeapon = EasyMock.createMock(Weapon.class);
+		weaponPrinter = new WeaponPrinter();
 	}
 	@Test
 	public void canPrintGear(){
+		EasyMock.expect(mockWeapon.getType()).andReturn("dagger");
+		EasyMock.expect(mockWeapon.getName()).andReturn("BeeStinger");
+		
+		EasyMock.replay(mockWeapon);
+		
+		String result = weaponPrinter.printGear(mockWeapon);
+		
+		Assert.assertEquals("Hey you used a weapon! dagger its name is BeeStinger", result);
+		
 		
 	}
 }
